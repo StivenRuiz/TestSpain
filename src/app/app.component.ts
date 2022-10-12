af6f865653cedaf7ec8fa3a1456bed186a7c52d6
mauricioriosg <riosguerreroa@gmail.com>
2022-10-11 21:53:42 -0500

    feat: event button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,87 @@
 import { Component } from '@angular/core';
-import { circleMarker, geoJSON, GeoJSONOptions, LatLng, LatLngBounds, Layer, Map, MapOptions, tileLayer, TileLayer } from 'leaflet';
+import {
+  circleMarker,
+  geoJSON,
+  GeoJSONOptions,
+  LatLng,
+  LatLngBounds,
+  Layer,
+  Map,
+  MapOptions,
+  tileLayer,
+  TileLayer,
+} from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent 
-{
-  public map:Map;
+export class AppComponent {
+  public map: Map;
 
-  public mapOptions:MapOptions = {
+  public mapOptions: MapOptions = {
     zoom: 17,
     zoomControl: false,
     center: [40.395347, -3.694041],
-    preferCanvas: true
+    preferCanvas: true,
   };
 
-  public baseLayer:TileLayer;
+  public baseLayer: TileLayer;
 
-  public  mapFitBounds:LatLngBounds = new LatLngBounds([
+  public mapFitBounds: LatLngBounds = new LatLngBounds([
     [37.50547228, -4.22810257],
-    [37.70590845000001, -3.98959274]
+    [37.70590845000001, -3.98959274],
   ]);
 
-  public constructor()
-  {
-    
-    this.baseLayer = tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      crossOrigin: 'anonymous',
-      className: 'OSM',
-      maxNativeZoom: 19,
-      maxZoom: 22,
-      minZoom: 5
-    });
-
+  public constructor() {
+    this.baseLayer = tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        crossOrigin: 'anonymous',
+        className: 'OSM',
+        maxNativeZoom: 19,
+        maxZoom: 22,
+        minZoom: 5,
+      }
+    );
   }
 
-  public onMapReady(map:Map):void
-  {
+  public onMapReady(map: Map): void {
     this.map = map;
     this.addLuminairesLayer();
   }
 
-  private async addLuminairesLayer():Promise<void>
-  {
-    const luminaires = await (await fetch('assets/data/luminarias.geojson')).json();
+  private async addLuminairesLayer(): Promise<void> {
+    const luminaires = await (
+      await fetch('assets/data/luminarias.geojson')
+    ).json();
 
-    const options:GeoJSONOptions = {
-      pointToLayer: (feature:GeoJSON.Feature, latLng:LatLng) => circleMarker(latLng),
-      style: feature =>  ({
-        radius: 8, 
-        color: "#333",
-        fillColor: "#FFFA4D",
+    const options: GeoJSONOptions = {
+      pointToLayer: (feature: GeoJSON.Feature, latLng: LatLng) =>
+        circleMarker(latLng),
+      style: (feature) => ({
+        radius: 8,
+        color: '#333',
+        fillColor: '#FFFA4D',
         weight: 1,
         opacity: 1,
-        fillOpacity: 1
-      }) 
+        fillOpacity: 1,
+      }),
     };
 
-    geoJSON(luminaires, options).addTo(this.map);
+    let l = geoJSON(luminaires, options)
+      .bindPopup('Hi There!')
+      .on('click', (event) => this.valida(event))
+      .addTo(this.map);
+  }
+
+  valida(event: any): void {
+    this.map.zoomIn();
+  }
+
+  mapClicked($event: any) {
+    // this.map.fitBounds($event.layer.getBounds());
+    console.log($event);
   }
 }
